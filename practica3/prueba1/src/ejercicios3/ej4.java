cad3356099f1bf9f7370db8c6d6e00fc76456b87
iván.villagra <56157806+ivanVillagralatorre@users.noreply.github.com>
2019-10-24 11:37:44 +0200

    Update ej4.java

--- practica3/prueba1/src/ejercicios3/ej4.java
+++ practica3/prueba1/src/ejercicios3/ej4.java
@@ -18,7 +18,9 @@ public class ej4 {
    public static void main(String[] args){
        int diasdif1;
        int diasdif2 ;
-   
+       
+  
+       try{
    int año1 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el el dia de la primera fecha"));
    int mes1 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el el mes de la primera fecha"));
    int dia1 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el el año de la primera fecha"));
@@ -29,18 +31,20 @@ public class ej4 {
    int año2 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el el dia de la segunda fecha"));
    int mes2 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el el mes de la segunda fecha"));
    int dia2 = Integer.parseInt(JOptionPane.showInputDialog("Escribe el el año  de la segunda fecha"));
-   
+ 
+       
    LocalDate date2 =LocalDate.of(dia2, mes2, año2);
  JOptionPane.showMessageDialog(null,date2);
-   
+     
   
     JOptionPane.showMessageDialog(null,date2.getDayOfYear()+" "+" "+date1.getDayOfYear());
     
    long c = java.time.temporal.ChronoUnit.DAYS.between(date1, date2);
     
     JOptionPane.showMessageDialog(null,c+"dias de diferencia entre la fecha 1 con la 2 ");
+    } catch(Exception e){JOptionPane.showMessageDialog(null, e.getMessage());}
     
-    }   
+      
 
    
    
