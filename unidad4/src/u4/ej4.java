79174454cff139ac6609bf0734b34ef9066d7ec9
iván.villagra <56157806+ivanVillagralatorre@users.noreply.github.com>
2019-11-07 11:53:16 +0100

    corregido

--- unidad4/src/u4/ej4.java
+++ unidad4/src/u4/ej4.java
@@ -12,57 +12,41 @@ import javax.swing.JOptionPane;
  * @author 1gdaw04
  */
 public class ej4 {
-    public static void main (String[]args){
-        String[] meses = new String [12];
-       
-            boolean valor=false;
-           dar(meses);
-           
-         
-           do{
-          String m=JOptionPane.showInputDialog("introduce el mes");
-           
-            df(valor,meses,m);
-           if(valor==false)
-          JOptionPane.showMessageDialog(null,"Mes incorrecto");
-            
-           }while(valor!=true);
-           
-           if(valor==true)
-           {JOptionPane.showMessageDialog(null,"Mes correcto");}
+
+
+    public static void main(String[] args) {
+        String[] meses = new String[12];
+
+        boolean valor = false;
+        meses[0] = "enero";
+        meses[1] = "febrero";
+        meses[2] = "marzo";
+        meses[3] = "abril";
+        meses[4] = "mayo";
+        meses[5] = "junio";
+        meses[6] = "julio";
+        meses[7] = "agosto";
+        meses[8] = "septiembre";
+        meses[9] = "octubre";
+        meses[10] = "noviembre";
+        meses[11] = "diciembre";
+
+         do{
+            String m = JOptionPane.showInputDialog("introduce el mes");
+            for (String i : meses) {
+                if (i.equalsIgnoreCase(m)) {
+                    valor = true;
+                }}
+                if (valor == false) {
+                    JOptionPane.showMessageDialog(null, "Mes incorrecto");
+                }
+            }while (valor != true);
+            JOptionPane.showMessageDialog(null, "Mes correcto");
+
+            //llenando
+        
+         }}
+    
+    
 
     
-    
-    
-    
-    }
-    
-     public static void dar(String[] meses){
-         //llenando
-          meses[0]="enero";
-          meses[1]="febrero";
-          meses[2]="marzo";
-          meses[3]="abril";
-          meses[4]="mayo";
-          meses[5]="junio";
-          meses[6]="julio";
-          meses[7]="agosto";
-          meses[8]="septiembre";
-          meses[9]="octubre";
-          meses[10]="noviembre";
-          meses[11]="diciembre";
-     
-     
-     }
-     
-     public static void df(boolean valor,String[]meses,String m){
-         for(int  i=0;i< meses.length  ; i++){
-           if(m.equalsIgnoreCase(meses[i]))
-           {valor=true;}
-     
-     }
-     }
-     
-    
-    
-}
